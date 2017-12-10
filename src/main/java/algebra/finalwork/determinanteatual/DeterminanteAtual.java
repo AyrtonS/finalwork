b3a8f447d62fa5d96fd2ed69b10f29671c0a6e84
Ariádna <ariadnamiranda022@gmail.com>
2017-12-10 09:07:05 -0300

    #31-Determinante generica finalizada

---
+++ src/main/java/algebra/finalwork/determinanteatual/DeterminanteAtual.java
@@ -0,0 +1,55 @@
+package algebra.finalwork.determinanteatual;
+
+import java.text.MessageFormat;
+
+public class DeterminanteAtual {
+	public static double matrixDeterminant (double[][] matrix) {
+		double temporary[][];
+		double result = 0;
+
+		if (matrix.length == 1) {
+			result = matrix[0][0];
+			return (result);
+		}
+
+		if (matrix.length == 2) {
+			result = ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
+			return (result);
+		}
+
+		for (int i = 0; i < matrix[0].length; i++) {
+			temporary = new double[matrix.length - 1][matrix[0].length - 1];
+
+			for (int j = 1; j < matrix.length; j++) {
+				for (int k = 0; k < matrix[0].length; k++) {
+					if (k < i) {
+						temporary[j - 1][k] = matrix[j][k];
+					} else if (k > i) {
+						temporary[j - 1][k - 1] = matrix[j][k];
+					}
+				}
+			}
+
+			result += matrix[0][i] * Math.pow (-1, (double) i) * matrixDeterminant (temporary);
+		}
+		return (result);
+	}
+
+ 
+    public static void main(String args[])
+    {
+    	
+    	double determinant;
+		double x[][] = {
+				{1,0, 5, 0,},
+				{2, -1, 0,3},
+				{3, 0, 2,0},
+				{7, 0, 6,5}
+				
+		};
+
+		DeterminanteAtual D = new DeterminanteAtual();
+		System.out.println (MessageFormat.format ("Determinant: {0}", Double.toString (D.matrixDeterminant(x))));
+	}
+
+}
